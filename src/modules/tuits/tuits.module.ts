d75d739378246fc0de3d16e995cd2df534135d14
Leonardo Farina <leonardoagustinfarina@gmail.com>
2022-07-11 11:08:56 -0300

    module implementation

---
+++ src/modules/tuits/tuits.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TuitsController } from './tuits.controller';
+import { TuitsService } from './tuits.service';
+
+@Module({
+    controllers: [TuitsController],
+    providers: [TuitsService],
+})
+export class TuitsModule {}
