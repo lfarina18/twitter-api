b7f75806d090aa55456e03d8cfb6694979aaaae7
Leonardo Farina <leonardoagustinfarina@gmail.com>
2022-07-12 11:18:41 -0300

    User added

--- src/modules/tuits/tuits.module.ts
+++ src/modules/tuits/tuits.module.ts
@@ -3,9 +3,10 @@ import { TuitsController } from './tuits.controller';
 import { TuitsService } from './tuits.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tuit } from './tuit.entity';
+import { User } from '../users/entities';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Tuit])],
+    imports: [TypeOrmModule.forFeature([Tuit, User])],
     controllers: [TuitsController],
     providers: [TuitsService],
 })
