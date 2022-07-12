dab822bb2970820a9a62e8aeb89c1e70266ae139
Leonardo Farina <leonardoagustinfarina@gmail.com>
2022-07-12 12:18:12 -0300

    . env added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TuitsModule } from './modules/tuits/tuits.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [TuitsModule, TypeOrmModule.forRoot({
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TuitsModule, 
+    TypeOrmModule.forRoot({
     type: 'postgres',
     host: 'localhost',
     port: 5432,
@@ -16,4 +20,13 @@ import { UsersModule } from './modules/users/users.module';
     synchronize: true,
   }), UsersModule],
 })
-export class AppModule { }
+export class AppModule { 
+
+  static port: number;
+
+  constructor(private readonly configService: ConfigService) {
+   AppModule.port = +this.configService.get('PORT') || 3000;
+  }
+    
+  
+}
